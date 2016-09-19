0a133232ee0653dd26d4ff98609215a8348af722
hemaassen <helgemaassen@yaoo.de>
2016-09-19 11:46:17 +0200

    2

--- app/src/main/java/com/example/helge/sensoren/MainActivity.java
+++ app/src/main/java/com/example/helge/sensoren/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.helge.sensoren;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.List;
@@ -29,6 +31,28 @@ public class MainActivity extends AppCompatActivity {
             textView.append(s.getName().toString() + "\n");
             textView.append(s.getVendor().toString() + "\n");
             textView.append(Integer.toString(s.getVersion()).toString() + "\n");
+        }
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if (sensor != null){
+            listener = new SensorEventListener() {
+                @Override
+                public void onSensorChanged(SensorEvent sensorEvent) {
+                    if (sensorEvent.values.length>0){
+                        float light = sensorEvent.values[0];
+                        String txt = Float.toString(light);
+                        textView.append(txt + " ");
+                    }else{
+                        textView.append("keine Änderung");
+                    }
+
+                }
+
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                    Toast.makeText(getApplicationContext(), "die Genauigkeit hat sich geändert auf " +
+                            accuracy, Toast.LENGTH_LONG).show();
+                }
+            };
         }
     }
 }
